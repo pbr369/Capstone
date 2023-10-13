e747605bec3b05a0cb0c08b6eee67f39bdb26496
pbr369 <143585864+pbr369@users.noreply.github.com>
2023-10-13 17:41:36 +0800

    first init

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+//import link
+import { Link } from "react-router-dom";
+//import icons
+import { IoMdClose } from "react-icons/io";
+
+export default function CartItem({ item }) {
+  //destructure item
+  const { id, title, image, price, amount } = item;
+  return (
+    <div className="flex">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px]" src={image} alt="" />
+        </Link>
+        <div className="w-full flex flex-col">
+          {/* title and remove icon */}
+          <div className="flex justify-between mb-2">
+            {/* title */}
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            {/* remove icon */}
+            <div>
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+//product provider
+import ProductProvider from './contexts/ProductContext.jsx';
+//sidebar provider
+import SidebarProvider from './contexts/SidebarContext.jsx';
+//import cart provider
+import CartProvider from './contexts/CartContext.jsx';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SidebarProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
+  </SidebarProvider>
+);

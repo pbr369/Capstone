14118736bfccdc9b4b349ad252bace33a4548d82
jepharelle cagas <cjepharlele@gmail.com>
2023-10-26 13:23:29 +0800

    added validation on login with error message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,100 @@
 import { Link } from "react-router-dom";
 import {BiUser} from "react-icons/bi";
 import { AiOutlineUnlock } from "react-icons/ai";
+import React, {useState,useEffect} from "react";
 
 const Login = () => {
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [loginErrors, setLoginErrors] = useState({});
+  
+    const validateLogin = () => {
+      let errors = {};
+  
+      if (!loginEmail) {
+        errors.email = "Please enter your email";
+      } else {
+        errors.email = "";
+      }
+  
+      if (!loginPassword) {
+        errors.password = "Please enter your password";
+      } else {
+        errors.password = "";
+      }
+  
+      setLoginErrors(errors);
+  
+      return Object.keys(errors).length === 0;
+    };
+  
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        validateLogin();
+      }, 100);
+  
+      return () => clearTimeout(timeout);
+    }, [loginEmail, loginPassword]);
+  
+    const handleLoginSubmit = (e) => {
+      e.preventDefault();
+    
+      const isValid = validateLogin();
+  
+      if (isValid) {
+        // submit form
+       
+      }
+    };
     return(
         <div className="text-white h-[100vh] flex justify-center bg-cover items-center bg-white">
         <div>
             <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg 
         backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
                  <h1 className="text-4xl text-black font-bold text-center mb-6">
-                    Login
+                    Register
                 </h1>
-             <form action="">
+             <form action="" onSubmit={handleLoginSubmit}>
              <div className="relative my-4">
                     <input type="email"    
                     className="block w-72 py-2.3 text-sm 
                     text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none
                     dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus-text-black
-                    focus:border-blue-600 peer " 
-                    placeholder=""/>
+                    focus:border-blue-600 peer "
+                    placeholder=""
+                    name="regsiter-email"
+                    id="register-email"
+                    value={loginEmail}
+                    onInput={(e) => setLoginEmail(e.target.value)} 
+                    />
 
                     <label htmlFor="" 
                     className="absolute text-sm text-black duration-300
                     transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0
                     peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100
                      peer-placeholder-shown:translate-y-0
-                    peer-focus:scale-75 peer-focus:-translate-y-6 
-                    ">
-                        Your Email</label>
-                    <BiUser className=" text-black absolute top-1 right-1"/>
-                </div>  
+                    peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Email</label>
+                    <BiUser className="text-black absolute top-1 right-1"/>
+                </div> 
+                {loginErrors.email && (
+                    <p className="text-sm text-red-500 -mt-3">
+                      {loginErrors.email}
+                    </p>
+                  )} 
                 <div className="relative my-4">
                     <input type="password" 
                     className="block w-72 py-2.3 text-sm 
                     text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none
                     dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black
                     focus:border-blue-600 peer " 
-                    placeholder=""/>
+                    placeholder=""
+                    name="register-password"
+                    id="register-password"
+                    value={loginPassword}
+                    onInput={(e) => setLoginPassword(e.target.value)}
+                    />
 
                     <label htmlFor="" className="absolute text-sm text-black duration-300
                     transform -translate-y-6 scale-75 top-1 -z-100 origin-[0] peer-focus:left-0
@@ -45,27 +103,26 @@ const Login = () => {
                      peer-placeholder-shown:translate-y-0
                     peer-focus:scale-75 peer-focus:-translate-y-6
                     ">
-                        Your Password</label>
+                         Password</label>
                         <AiOutlineUnlock className="text-black absolute top-1 right-1"/>
                 </div> 
-                <div className="flex justify-between items-center">
-                    <div className="flex gap-2 items-center text-black">
-                       <input type="Checkbox" checkbox ="" id="" /> 
-                       <label htmlFor="Remember Me">Remember me</label>
-                    </div>
-                    <Link to ='' className="text-blue-500">Forgot Password?</Link>
-                </div>  
+                {loginErrors.password && (
+                    <p className="text-sm text-red-500 -mt-3">
+                      {loginErrors.password}
+                    </p>
+                  )}
                 <button className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-black
                 hover:bg-black hover:text-white py-2 transition-color duration-300" 
-                type = "submit">Login</button>
+                type ="submit">Create an Account</button>
+                
                 <div>
-                    <span className="m-4 text-black">New Here? <Link to='/Register' className="text-blue-500">Create an Account</Link></span> 
+                    <span className="m-12 text-black  ">New here? <Link to='/Register' className="text-blue-500">Create an Account</Link></span> 
                 </div>
              </form>
             </div>
         </div>
         </div>
-    );;
+    );
 };
 
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,70 @@
 import { Link } from "react-router-dom";
 import {BiUser} from "react-icons/bi";
 import { AiOutlineUnlock } from "react-icons/ai";
+import React, {useState,useEffect} from "react";
 
 const Login = () => {
+    
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("")
+    const [registerErrors, setRegisterErrors] = useState({});
+
+   
+
+    
+    const validateRegister = () => {
+      let errors = {};
+  
+
+      const emailreg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      if (!registerEmail) {
+        errors.email = "Please enter a valid email address";
+      } else if (!emailreg.test(registerEmail)) {
+        errors.email = "Include @ on the email address";
+      } else {
+        errors.email = "";
+      }
+  
+      if (!registerPassword) {
+        errors.password = "Create a unique password ";
+      } else if (registerPassword.length < 8) {
+        errors.password = "Passwords must be at least 8 characters long";
+      } else {
+        errors.password = "";
+      }
+
+      if (confirmPassword != registerPassword) {
+        errors.confirmPassword = "Confirm Password ";
+      }
+     
+  
+      setRegisterErrors(errors);
+  
+      return Object.keys(errors).length === 0;
+    };
+  
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        validateRegister();
+      }, 100);
+  
+      return () => clearTimeout(timeout);
+    }, [, registerEmail, registerPassword]);
+  
+    const handleRegisterSubmit = (e) => {
+      e.preventDefault();
+    
+    
+      const isValid = validateRegister();
+  
+      if (isValid) {
+        // submit form
+        
+      }
+   
+    };
+  
     return(
         <div className="text-white h-[100vh] flex justify-center bg-cover items-center bg-white">
         <div>
@@ -11,32 +73,47 @@ const Login = () => {
                  <h1 className="text-4xl text-black font-bold text-center mb-6">
                     Register
                 </h1>
-             <form action="">
+             <form action="" onSubmit={handleRegisterSubmit}>
              <div className="relative my-4">
                     <input type="email"    
                     className="block w-72 py-2.3 text-sm 
                     text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none
                     dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus-text-black
-                    focus:border-blue-600 peer " 
-                    placeholder=""/>
+                    focus:border-blue-600 peer "
+                    placeholder=""
+                    name="regsiter-email"
+                    id="register-email"
+                    value={registerEmail}
+                    onInput={(e) => setRegisterEmail(e.target.value)} 
+                    />
 
                     <label htmlFor="" 
                     className="absolute text-sm text-black duration-300
                     transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0
                     peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100
                      peer-placeholder-shown:translate-y-0
-                    peer-focus:scale-75 peer-focus:-translate-y-6 
-                    ">
-                        Your Email</label>
+                    peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Register Email</label>
                     <BiUser className="text-black absolute top-1 right-1"/>
-                </div>  
+                </div> 
+                {registerErrors.email && (
+                    <p className="text-sm text-red-500 -mt-3">
+                      {registerErrors.email}
+                    </p>
+                  )} 
                 <div className="relative my-4">
                     <input type="password" 
                     className="block w-72 py-2.3 text-sm 
                     text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none
                     dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black
                     focus:border-blue-600 peer " 
-                    placeholder=""/>
+                    placeholder=""
+                    name="register-password"
+                    id="register-password"
+                    value={registerPassword}
+                    onInput={(e) => setRegisterPassword(e.target.value)}
+                    />
 
                     <label htmlFor="" className="absolute text-sm text-black duration-300
                     transform -translate-y-6 scale-75 top-1 -z-100 origin-[0] peer-focus:left-0
@@ -48,13 +125,24 @@ const Login = () => {
                         Your Password</label>
                         <AiOutlineUnlock className="text-black absolute top-1 right-1"/>
                 </div> 
-                <div className="relative my-4">
+                {registerErrors.password && (
+                    <p className="text-sm text-red-500 -mt-3">
+                      {registerErrors.password}
+                    </p>
+                  )}
+              
+              <div className="relative my-4">
                     <input type="password" 
                     className="block w-72 py-2.3 text-sm 
                     text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none
                     dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black
                     focus:border-blue-600 peer " 
-                    placeholder=""/>
+                    placeholder=""
+                    name="register-password"
+                    id="register-password"
+                    value={confirmPassword}
+                    onInput={(e) => setconfirmPassword(e.target.value)}
+                    />
 
                     <label htmlFor="" className="absolute text-sm text-black duration-300
                     transform -translate-y-6 scale-75 top-1 -z-100 origin-[0] peer-focus:left-0
@@ -66,9 +154,16 @@ const Login = () => {
                         Confirm Password</label>
                         <AiOutlineUnlock className="text-black absolute top-1 right-1"/>
                 </div> 
-                <button className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-black
+                {registerErrors.confirmPassword && (
+                    <p className="text-sm text-red-500 -mt-3">
+                      {registerErrors.confirmPassword}
+                    </p>
+                  )}
+               
+                <button value="Send" className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-black
                 hover:bg-black hover:text-white py-2 transition-color duration-300" 
-                type = "submit">Login</button>
+                type = "submit">Create an Account</button>
+                
                 <div>
                     <span className="m-4 text-black">Already Create an Account? <Link to='/Login' className="text-blue-500">Login</Link></span> 
                 </div>
@@ -76,7 +171,7 @@ const Login = () => {
             </div>
         </div>
         </div>
-    );;
+    );
 };
 
 export default Login
